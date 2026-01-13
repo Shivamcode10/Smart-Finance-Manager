db639589859a9ef81e9fa1cf0f37a748bdaddf74
Shivamcode10 <shivamlahane2210@gmail.com>
2026-01-14 01:58:49 +0530

    Fix React mount for GitHub Pages blank page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-
 import App from "./App";
 import "./index.css";
 import "./App.css";
@@ -9,7 +8,9 @@ import "./App.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
