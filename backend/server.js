93be243aeaca9bf4021249b64fc8c2ffc2502375
Shivamcode10 <shivamlahane2210@gmail.com>
2026-02-06 03:42:56 +0530

    made

--- backend/server.js
+++ backend/server.js
@@ -69,10 +69,20 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 
 
 app.get("/", (req, res) => {
